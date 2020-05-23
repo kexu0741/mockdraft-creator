399b3f185d06c0a57e08c69b13785b3c715a2a74
Kevin Xu <kexu0741@colorado.edu>
2020-05-23 17:08:47 -0600

    working on connecting db

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
 
+const cors = require("cors");
+const pool = require("./db");
+
+app.get("/", async(req, res) => {
+	try {
+		const test = await pool.query("SELECT * FROM users;");
+		res.json(test.rows);
+
+	} catch (err){
+		console.log(err.message)
+	}
+})
+
 
 var port = process.env.port || 5000;
 app.listen(port, () => {
