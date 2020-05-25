b79e5b1eb8256f4230a6f82f068a4d9d5bc5ad59
Kevin Xu <kexu0741@colorado.edu>
2020-05-24 20:34:15 -0600

    adding inital react files

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
 
+const path = require("path");
 const cors = require("cors");
 const pool = require("./db");
 const bcrypt = require("bcrypt");
 
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get("/", async(req, res) => {
 	try {
