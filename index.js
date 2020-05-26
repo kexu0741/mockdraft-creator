97f70d7306f276cb12bebb783eaffa540cf102b7
Kevin Xu <kexu0741@colorado.edu>
2020-05-25 23:14:30 -0600

    set up token generation

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const cors = require("cors");
 const pool = require("./db");
 const bcrypt = require("bcrypt");
+const jwtGenerator = require("./utils/jwtGenerator");
 
 app.use(cors());
 app.use(express.json());
@@ -15,6 +16,7 @@ app.get("/", async(req, res) => {
 	res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
+// register route
 app.post("/register", async(req, res) => {
 	try {  
 		let hashed_pass = await bcrypt.hash(req.body.password, 10);
@@ -22,11 +24,38 @@ app.post("/register", async(req, res) => {
 
 		const new_entry = await pool.query("INSERT INTO users(email, password) VALUES($1, $2) RETURNING *", 
 		[email, hashed_pass]);
-		res.json(new_entry.rows[0]);
+
+		const token = jwtGenerator(new_entry.rows[0].uid);
+
+		res.json({token});
 	} catch (err){ 
 		// if a duplicate email is entered, we come here
 		res.status(500).send({ error: "boo:(" });
 		console.log(err.message);
+	}
+});
+
+// login route
+app.post("/login", async(req, res) => {
+	try {
+		let email = req.body.email.toLowerCase();
+		const user = await pool.query("SELECT * FROM users WHERE email = $1", 
+			[email]);
+
+		if (user.rows.length === 0){
+			// user is not found
+			return res.status(401).json("Incorrect credentials"); // unauthenticated
+		}
+
+		const valid_pass = await bcrypt.compare(req.body.password, user.rows[0].password);
+		if (!valid_pass){
+			return res.status(401).json("Incorrect credentials")
+		}
+
+		const token = jwtGenerator(user.rows[0].uid);
+	} catch (err){
+		console.log(err.message);
+
 	}
 });
 
